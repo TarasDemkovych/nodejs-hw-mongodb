f3837061c7450e470bcc61086e39e4398403dd99
1010 <uafenomeno@gmail.com>
2025-10-07 02:38:51 +0200

    hw-3

--- src/server.js
+++ src/server.js
@@ -1,28 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import contactsRouter from './routes/contactsRouter.js';
+
+import contactsRouter from './routes/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+const PORT = process.env.PORT || 3000;
+
 export const setupServer = () => {
   const app = express();
-  app.get('/', (req, res) => {
-    // res.json();
-    res.redirect('/contacts');
-  });
-
-  app.use(cors());
-  app.use(pino());
 
   app.use(express.json());
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-  app.use('/contacts', contactsRouter);
-
-  app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
-  });
-
-  const PORT = process.env.PORT || 3000;
+  app.use(contactsRouter);
+  app.use('/', notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
   });
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,42 @@
-import { Contact } from '../models/contact.js';
+import { ContactsCollection } from '../db/models/contact.js';
 
-export const getAllContactsService = async () => {
-  const contacts = await Contact.find();
+export const getAllContacts = async () => {
+  const contacts = await ContactsCollection.find();
   return contacts;
 };
 
-export const getContactByIdService = async (contactId) => {
-  const contact = await Contact.findById(contactId);
+export const getContactById = async (contactId) => {
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload, option = {}) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...option,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
   return contact;
 };
